9dfcd72f2fd123e2430309438f1e5138819fbd66
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2023-10-22 15:56:21 +0300

    elasticsearch query types initial commit

---
+++ src/main/java/com/yunusbagriyanik/elasticsearchquerytypes/ElasticsearchQueryTypesApplication.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik.elasticsearchquerytypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchQueryTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchQueryTypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunusbagriyanik/elasticsearchquerytypes/ElasticsearchQueryTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunusbagriyanik.elasticsearchquerytypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchQueryTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
